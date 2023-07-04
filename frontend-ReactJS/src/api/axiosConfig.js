5ab37c8103c05b586621bfb2d3dcfd4d576287f3
Shaji-Mohammed <mshajisufiyan@gmail.com>
2023-07-04 13:55:53 -0300

    created header and added css

--- frontend-ReactJS/src/api/axiosConfig.js
+++ frontend-ReactJS/src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://019a-134-41-13-2.ngrok-free.app",
+  baseURL: "https://7835-134-190-176-235.ngrok-free.app",
   headers: { "ngrok-skip-browser-warning": "true" },
 });
