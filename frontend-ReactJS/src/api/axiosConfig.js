392c6707c1c97bfce188abfe50da56b74f7e5231
Shaji-Mohammed <mshajisufiyan@gmail.com>
2023-07-04 21:00:55 -0300

    creater Trailer component and updated the rest

--- frontend-ReactJS/src/api/axiosConfig.js
+++ frontend-ReactJS/src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://7835-134-190-176-235.ngrok-free.app",
+  baseURL: "https://01d4-134-41-13-2.ngrok-free.app",
   headers: { "ngrok-skip-browser-warning": "true" },
 });
